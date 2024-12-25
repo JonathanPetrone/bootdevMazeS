0e4c88b8943a159ed42ece463b2e327d0a5e42b1
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2024-12-25 21:26:10 +0100

    added maze

--- src/main.py
+++ src/main.py
@@ -2,22 +2,9 @@ from window import *
 
 def main():
     print("Maze solver")
-    # Create a window instance
-    win = Window(400, 400)
-
-    # Define points for your cells
-    top_left = Point(50, 50)
-    bottom_right = Point(100, 100)
-
-    # Create one or more cell instances
-    cell1 = Cell(top_left, bottom_right, win)
-    cell2 = Cell(Point(100, 50), Point(150, 100), win)
-
-    # Draw your cell(s) on the canvas
-    cell1.draw()
-    cell2.draw()
-
-    # Hold the window open until closed by the user
+    
+    win = Window(800, 600)
+    maze = Maze(100, 100, 10, 10, 50, 50, win)
     win.wait_for_close()
 
 main()
--- src/window.py
+++ src/window.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Window:
     def __init__(self, width, height):
@@ -48,6 +49,7 @@ class Line:
             x1, y1, x2, y2, fill=fill_color, width=2
         )
 
+
 class Cell:
     def __init__(self, point_1, point_2, win):
         self.has_left_wall = True
@@ -59,6 +61,7 @@ class Cell:
         self._y1 = point_1.y
         self._y2 = point_2.y
         self._win = win
+        CELL_SIZE = 50
 
     def draw(self):
         if self.has_left_wall:
@@ -76,3 +79,66 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line, "black")
+    
+    def draw_move(self, to_cell, undo=False):
+        val_cell_one_x = (self._x1 + self._x2) / 2
+        val_cell_one_y = (self._y1 + self._y2) / 2
+        val_cell_two_x = (to_cell._x1 + to_cell._x2) / 2
+        val_cell_two_y = (to_cell._y1 + to_cell._y2) / 2
+        line = Line(Point(val_cell_one_x, val_cell_one_y), Point(val_cell_two_x, val_cell_two_y))
+        
+        if undo:
+            self._win.draw_line(line, "gray")
+        else:
+            self._win.draw_line(line, "red")
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+    # First create all cells
+        for i in range(self.num_rows):
+            row = []
+            for j in range(self.num_cols):
+                x1 = self.x1 + (j * self.cell_size_x)
+                y1 = self.y1 + (i * self.cell_size_y)
+                x2 = self.x1 + ((j + 1) * self.cell_size_x)
+                y2 = self.y1 + ((i + 1) * self.cell_size_y)
+                new_cell = Cell(Point(x1, y1), Point(x2, y2), self.win)
+                row.append(new_cell)
+            self._cells.append(row)
+
+        # Then draw all cells
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self._draw_cell(i, j)
+    
+    def _draw_cell(self, i, j):
+        cell = self._cells[i][j]
+        cell.draw()
+        self._animate()
+
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+
+
